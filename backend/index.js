c6a0d7ee1ede534c636ae43406dee982c27c426a
Akash <akashmahendrakar6073@gmail.com>
2024-09-28 11:20:22 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const app=express();
-const port=4000;
+const port=process.env.PORT;
 const userModel = require('./Models/userModel')
 const route = require('./UserRoutes/UserRoutes')
 const cors = require('cors')
